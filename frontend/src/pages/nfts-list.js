8976bf1283107189dbfa01af79bf2478e9e41480
best-lucky1030 <bestlucky1030@gmail.com>
2022-02-07 11:02:57 +0300

    add front-end features

--- frontend/src/pages/nfts-list.js
+++ frontend/src/pages/nfts-list.js
@@ -1,42 +1,37 @@
-import { useState, useEffect } from "react"
 import Head from "next/head"
-import HomePage from "../components/HomePage"
+import { useEffect, useState } from "react"
+import NFTMap from "../components/NFTMap"
 import Web3Modal from "web3modal"
 import Web3 from "web3"
 import { CHAIN_ID, SITE_ERROR, SMARTCONTRACT_ABI, SMARTCONTRACT_ABI_ERC20, SMARTCONTRACT_ADDRESS, SMARTCONTRACT_ADDRESS_ERC20 } from "../../config"
+import { ethers, providers } from "ethers"
 import Sidebar from "../components/Sidebar"
 import MainContent from "../components/MainContent"
 import Header from "../components/Header"
-import { ethers, providers } from "ethers"
-import { errorAlert, errorAlertCenter } from "../components/toastGroup"
 import Moralis from "moralis"
 import MobileFooter from "../components/MobileFooter"
+import { errorAlert, errorAlertCenter } from "../components/toastGroup"
 import { providerOptions } from "../hook/connectWallet"
 import { checkNetwork, getNFTsTransfers } from "../hook/ethereum"
 import UnderstandModal from "../components/UnderstandModal"
 
-let web3Modal = undefined
+export default function NFTLIST({
+  startLoading,
+  closeLoading,
+  headerAlert,
+  closeAlert,
+  ...props
+}) {
 
-export default function Home({ headerAlert, closeAlert, closeLoading }) {
-
-  const [totalReward, setTotalReward] = useState(0)
-  const [loading, setLoading] = useState(false)
-
-  const [stakedCnt, setStakedCnt] = useState(0)
-  const [unstakedCnt, setUnstakedCnt] = useState(0)
-
+  let stakedNfts = []
+  const [filterState, setFilterState] = useState(2)
+  const [stakedList, setStakedList] = useState([])
+  const [checkAble, setCheckAble] = useState(false)
   const [connected, setConnected] = useState(false)
   const [signerAddress, setSignerAddress] = useState("")
+  const [currentSigner, setCurrentSigner] = useState()
   const [signerBalance, setSignerBalance] = useState(0)
-  const [totalSupply, setTotalSupply] = useState(0)
-  const [totalDusty, setTotalDusty] = useState(0)
-  const [staked, setStaked] = useState(0)
-  const [earlyRemoved, setEarlyRemoved] = useState(0)
-  const [dbalance, setdBalance] = useState(0)
-  const [holders, setHolders] = useState(0)
-  const [ownerDusty, setTotalOwnerDusty] = useState(0)
-  const [homeLoading, setHomeloading] = useState(false)
-  const [totalNFTs, setTotalNFTs] = useState(0)
+  const [loading, setLoading] = useState(false)
 
   const [autoTokenAddress, setAutoTokenAddress] = useState("")
   const [autoTokenId, setAutoTokenId] = useState("")
@@ -44,28 +39,21 @@ export default function Home({ headerAlert, closeAlert, closeLoading }) {
   const [underModal, setUnderModal] = useState(false)
 
   const connectWallet = async () => {
+    setLoading(true)
     if (await checkNetwork()) {
-      web3Modal = new Web3Modal({
+      const web3Modal = new Web3Modal({
         network: 'mainnet', // optional
         cacheProvider: true,
         providerOptions, // required
       })
-      setHomeloading(true) //loading start
-      setLoading(true)
       const provider = await web3Modal.connect()
       const web3Provider = new providers.Web3Provider(provider)
-
       const signer = web3Provider.getSigner()
+      setCurrentSigner(signer)
       const address = await signer.getAddress()
 
       setConnected(true)
       setSignerAddress(address)
-
-      const contract = new ethers.Contract(
-        SMARTCONTRACT_ADDRESS,
-        SMARTCONTRACT_ABI,
-        signer
-      )
 
       const contract_20 = new ethers.Contract(
         SMARTCONTRACT_ADDRESS_ERC20,
@@ -74,33 +62,10 @@ export default function Home({ headerAlert, closeAlert, closeLoading }) {
       )
       const bal = await contract_20.balanceOf(address)
       setSignerBalance(ethers.utils.formatEther(bal))
-
-      const totalS = await contract_20.totalSupply()
-      setTotalSupply(ethers.utils.formatEther(totalS))
-
-      const totlass = await contract_20.holders()
-      setHolders(totlass.toString())
-
-      const early = await contract.earlyRemoved()
-      setEarlyRemoved(early.toString())
-
-      const totalN = await contract_20.balanceOf(SMARTCONTRACT_ADDRESS)
-      setTotalDusty(totalN.toString())
-
-      const Obal = await contract.bonusPool()
-      setTotalOwnerDusty(parseFloat(Obal.toString()) + parseFloat(1114))
-
-      const sta = await contract.totalStaked()
-      setStaked(sta.toString())
-
-      setHomeloading(false) //loading off
-
-      // Subscribe to accounts change
+      setLoading(false)
       provider.on("accountsChanged", (accounts) => {
         setSignerAddress(accounts[0])
       })
-
-      // Subscribe to chainId change
       provider.on("chainChanged", (chainId) => {
         window.location.reload()
       })
@@ -108,9 +73,10 @@ export default function Home({ headerAlert, closeAlert, closeLoading }) {
   }
 
   const setPastNFTs = async () => {
-    setLoading(true)
+    startLoading()
     const web3 = new Web3(Web3.givenProvider)
     const accounts = await web3.eth.getAccounts()
+
     const web3Modal = new Web3Modal({
       network: 'mainnet', // optional
       cacheProvider: true,
@@ -124,35 +90,32 @@ export default function Home({ headerAlert, closeAlert, closeLoading }) {
       SMARTCONTRACT_ABI,
       signer
     )
-    let Nfts = []
-    let sCnt = 0
-    let usCnt = 0
-    let rewords = 0
+    stakedNfts = []
     const userNFTs = await Moralis.Web3API.account.getNFTs({ chain: 'bsc', address: accounts[0] })
-
     const nftTransfers = await getNFTsTransfers()
-
-    for (var i = 0; i < userNFTs.result.length; i++) {
-      const nftDump = await contract.status(accounts[0], userNFTs.result[i].token_address, userNFTs.result[i].token_id)
-      if (nftDump.action === 1) {
-        tossingCheck(userNFTs.result[i].token_address, userNFTs.result[i].token_id, nftDump.stakedTime.toString(), nftTransfers)
+    if (userNFTs.total !== 0) {
+      startLoading()
+      for (var i = 0; i < userNFTs.result.length; i++) {
+        const nftDump = await contract.status(accounts[0], userNFTs.result[i].token_address, userNFTs.result[i].token_id)
+        if (nftDump.action === 1) {
+          tossingCheck(userNFTs.result[i].token_address, userNFTs.result[i].token_id, nftDump.stakedTime.toString(), nftTransfers)
+        }
+        stakedNfts.push({
+          name: userNFTs.result[i].name,
+          action: nftDump.action,
+          token_address: userNFTs.result[i].token_address,
+          token_id: userNFTs.result[i].token_id,
+          reward: nftDump.reward,
+          timestamp: nftDump.stakedTime.toString(),
+          percent: nftDump.percent,
+          token_uri: userNFTs.result[i].token_uri,
+        })
       }
-      Nfts.push({
-        action: nftDump.action,
-        token_address: userNFTs.result[i].token_address,
-        token_id: userNFTs.result[i].token_id,
-        reward: nftDump.reward,
-        timestamp: nftDump.stakedTime.toString(),
-      })
-      nftDump.action === 1 ? sCnt++ : usCnt++
-      rewords = rewords + parseFloat(ethers.utils.formatEther(nftDump.reward))
+      closeLoading()
+      setStakedList(stakedNfts)
+    } else {
+      closeLoading()
     }
-
-    setTotalNFTs(Nfts.length)
-    setStakedCnt(sCnt)
-    setUnstakedCnt(usCnt)
-    setTotalReward(rewords)
-    setLoading(false)
   }
 
   const tossingCheck = (tokenAddress, tokenId, timeStamp, nftTransfers) => {
@@ -178,14 +141,19 @@ export default function Home({ headerAlert, closeAlert, closeLoading }) {
     setUnderModal(true)
   }
 
+  const getNFTLIST = () => {
+    startLoading()
+    setPastNFTs()
+    // setStakedNFTs()
+  }
+
   useEffect(() => {
     closeLoading()
     async function fetchData() {
       if (typeof window.ethereum !== 'undefined') {
         if (await checkNetwork("no-alert")) {
-          // setLoading(true)
-          await connectWallet()
-          setPastNFTs()
+          connectWallet()
+          getNFTLIST()
           ethereum.on('accountsChanged', function (accounts) {
             window.location.reload()
           })
@@ -217,7 +185,7 @@ export default function Home({ headerAlert, closeAlert, closeLoading }) {
         connectWallet={connectWallet}
         connected={connected}
         signerBalance={signerBalance}
-        loading={homeLoading}
+        loading={loading}
         headerAlert={headerAlert}
         closeAlert={closeAlert}
       />
@@ -226,28 +194,26 @@ export default function Home({ headerAlert, closeAlert, closeLoading }) {
           connected={connected}
           headerAlert={headerAlert}
         />
-        <div className="page-content">
+        <div className='page-content'>
           <Head>
-            <title>Dusty Vaults | Home</title>
+            <title>Dusty Vaults | NFTs List</title>
             <meta name="description" content="NFT Bank" />
             <link rel="icon" href="/favicon.ico" />
           </Head>
-          <HomePage
-            connected={connected}
-            totalSupply={totalSupply}
-            staked={staked}
-            earlyRemoved={earlyRemoved}
-            dbalance={dbalance}
-            homeLoading={homeLoading}
+          <NFTMap
             address={signerAddress}
-            totalDusty={totalDusty}
-            ownerDusty={ownerDusty}
-            holders={holders}
-            stakedCnt={stakedCnt}
-            unstakedCnt={unstakedCnt}
-            totalNFTs={totalNFTs}
-            totalReward={totalReward}
-            loading={loading}
+            signer={currentSigner}
+            setForce={(e) => setForceRender(e)}
+            filterState={filterState}
+            setFilterState={(e) => setFilterState(e)}
+            checkAble={checkAble}
+            setCheckAble={(e) => setCheckAble(e)}
+            getNFTLIST={() => getNFTLIST()}
+            stakedList={stakedList}
+            startLoading={startLoading}
+            closeLoading={closeLoading}
+            headerAlert={headerAlert}
+            balance={signerBalance}
           />
         </div>
       </MainContent>
